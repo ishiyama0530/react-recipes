150bdc7f7d7874ce73ec67f3f11ea14d8c2efdf4
Masanobu Ishiyama <ishiyama0530.dev@gmail.com>
2020-02-02 17:22:41 +0900

    add graphql page.

--- src/pages/graphql/index.tsx
+++ src/pages/graphql/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
-// import { makeStyles, Theme } from '@material-ui/core'
+import {
+  makeStyles,
+  Theme,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Container,
+  Typography
+} from '@material-ui/core'
 import { gql } from 'apollo-boost'
 import { useQuery } from '@apollo/react-hooks'
+import Paper from '@material-ui/core/Paper'
+import HomeTemplate from '../../components/templates/HomeTemplate'
 
 type Props = { children?: never }
 
@@ -14,22 +27,81 @@ const EXCHANGE_RATES = gql`
   }
 `
 
-// const useStyles = makeStyles((theme: Theme) => ({}))
+const useStyles = makeStyles((theme: Theme) => ({
+  sub: {
+    margin: theme.spacing(4)
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6)
+  },
+  tableContainer: {
+    padding: theme.spacing(4)
+  }
+}))
 
 const GraphQL: React.FC<Props> = () => {
-  // const classes = useStyles()
+  const classes = useStyles()
   const { loading, error, data } = useQuery(EXCHANGE_RATES)
 
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error :(</p>
+  if (loading) return <p className={classes.sub}>Loading...</p>
+  if (error) return <p className={classes.sub}>Error: {error}</p>
 
-  return data.rates.map(({ currency, rate }: any) => (
-    <div key={currency}>
-      <p>
-        {currency}: {rate}
-      </p>
-    </div>
-  ))
+  return (
+    <HomeTemplate>
+      <div className={classes.heroContent}>
+        <Container maxWidth="lg">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="textPrimary"
+            gutterBottom
+          >
+            Use Apollo to get from free API.
+          </Typography>
+          <Typography
+            component="p"
+            variant="body1"
+            align="center"
+            color="textPrimary"
+            gutterBottom
+          >
+            powered by{' '}
+            <a
+              href="https://48p1r2roz4.sse.codesandbox.io/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Playground
+            </a>
+          </Typography>
+        </Container>
+      </div>
+      <Container maxWidth="xs" className={classes.tableContainer}>
+        <TableContainer component={Paper}>
+          <Table size="small" aria-label="a currency table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">CURRENCY</TableCell>
+                <TableCell align="right">RATE</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.rates.map(({ currency, rate }: any) => (
+                <TableRow key={currency} hover>
+                  <TableCell component="th" scope="row" align="center">
+                    {currency}
+                  </TableCell>
+                  <TableCell align="right">{rate}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
+    </HomeTemplate>
+  )
 }
 
 export default GraphQL
